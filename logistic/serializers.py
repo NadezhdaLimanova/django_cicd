368975c2c40a84e36438c242846e7b5fe7f0d865
Nadezhda Limanova <dindinka86@gmail.com>
2023-12-08 14:07:16 +0300

    added ci.yml

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -1,22 +1,16 @@
 from rest_framework import serializers
-
 from logistic.models import Product
-
 from logistic.models import Stock
-
 from logistic.models import StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
         fields = ['id', 'title', 'description']
 
 
-
 class ProductPositionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = StockProduct
         fields = ['id', 'product', 'quantity', 'price']
@@ -29,34 +23,23 @@ class StockSerializer(serializers.ModelSerializer):
         model = Stock
         fields = ['id', 'address', 'positions']
 
-
-
     def create(self, validated_data):
-
         positions = validated_data.pop('positions')
-
         stock = super().create(validated_data)
-
         for i in positions:
             StockProduct.objects.create(
                 stock=stock,
                 product=i['product'],
                 quantity=i['quantity'],
                 price=i['price'])
-
         return stock
 
     def update(self, instance, validated_data):
-
         positions = validated_data.pop('positions')
-
         stock = super().update(instance, validated_data)
-
-
         for i in positions:
             StockProduct.objects.update_or_create(
                 stock=stock,
                 product=i['product'],
                 defaults={'quantity': i['quantity'], 'price': i['price']})
-
         return stock
